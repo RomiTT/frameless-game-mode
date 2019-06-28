baff4d7e20c2c55736cb7e6d631cd56fc8b6b1f2
RomiTT <chikyom.kim@gmail.com>
2019-06-28 09:11:09 +0900

    Updated the script.

--- native_addons/frameless_game_mode/scripts/copyToPublic.js
+++ native_addons/frameless_game_mode/scripts/copyToPublic.js
@@ -3,5 +3,5 @@ const { copyFileSync } = require('fs');
 let src = './build/Release/fgm.node';
 let dest = '../../public/fgm.node';
 
-console.log(`Copying '${src}' to '${dest}'`);
 copyFileSync(src, dest);
+console.log(`Copied '${src}' to '${dest}'`);
