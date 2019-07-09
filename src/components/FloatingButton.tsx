6e23c52678ae75b7684057806eded4f0e8c46896
RomiTT <chikyom.kim@gmail.com>
2019-07-09 22:16:01 +0900

    Implement FloatingButton component.

---
+++ src/components/FloatingButton.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, MaybeElement, IconName, Intent } from '@blueprintjs/core';
+
+interface FloatingButtonProps {
+  position?: 'fixed' | 'absolute';
+  left?: number;
+  top?: number;
+  text?: string;
+  icon?: IconName | MaybeElement;
+  intent?: Intent;
+  scale?: number;
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export default class FloatingButton extends React.Component<
+  FloatingButtonProps,
+  object
+> {
+  static defaultProps = {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    scale: 1.0
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          position: this.props.position,
+          left: this.props.left,
+          top: this.props.top,
+          transform: `scale(${this.props.scale})`
+        }}
+      >
+        <Button
+          text={this.props.text}
+          icon={this.props.icon}
+          intent={this.props.intent}
+          onClick={this.props.onClick}
+        />
+      </div>
+    );
+  }
+}
