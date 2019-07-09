f1134f8b8fb587381bc69ddc535b88858007c3ac
RomiTT <chikyom.kim@gmail.com>
2019-07-09 22:16:17 +0900

    Update code.

--- src/components/WindowAppList.tsx
+++ src/components/WindowAppList.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { autorun } from 'mobx';
 import { inject } from 'mobx-react';
-import { Colors, H4, H5 } from '@blueprintjs/core';
+import { Button, Colors, H4, H5, Icon } from '@blueprintjs/core';
 import { IStoreFGM } from '../stores/StoreFGM';
 import styles from './WindowAppList.module.scss';
+import FloatingButton from './FloatingButton';
 
 interface WindowAppListProps {
   storeFGM?: IStoreFGM;
@@ -13,7 +14,6 @@ interface WindowAppListProps {
 }
 
 interface WindowsAppListState {
-  listData: Array<object>;
   selectedIndex: number;
 }
 
@@ -28,32 +28,49 @@ class WindowAppList extends React.Component<
   };
 
   timerId: any;
+  listRef: React.RefObject<HTMLDivElement>;
 
   constructor(props: WindowAppListProps) {
     super(props);
 
     this.state = {
-      listData: new Array<object>(),
       selectedIndex: -1
     };
 
     this.timerId = 0;
+    this.listRef = React.createRef();
   }
 
-  componentDidMount() {
-    this.timerId = setInterval(() => {
-      this.props.storeFGM!.updateWindowAppList((list: Array<object>) => {
-        this.setState({ listData: list });
-      });
-    }, this.props.storeFGM!.intervalToUpdateList);
-  }
+  componentDidMount() {}
 
   componentWillUnmount() {
     clearInterval(this.timerId);
   }
 
+  handleKeyDown = (e: any) => {
+    // Arrow up
+    if (e.keyCode === 38) {
+      if (this.state.selectedIndex > 0) {
+        let newIndex = this.state.selectedIndex - 1;
+        console.log('newIndex=', newIndex);
+        this.setState({ selectedIndex: newIndex });
+      }
+    }
+    // Arrow Down
+    else if (e.keyCode == 40) {
+      if (
+        this.state.selectedIndex <
+        this.props.storeFGM!.listAppToMonitor.length - 1
+      ) {
+        let newIndex = this.state.selectedIndex + 1;
+        console.log('newIndex=', newIndex);
+        this.setState({ selectedIndex: newIndex });
+      }
+    }
+  };
+
   renderItem = (index: number) => {
-    const item: any = this.state.listData[index];
+    const item: any = this.props.storeFGM!.listAppToMonitor[index];
     const classes =
       this.state.selectedIndex === index
         ? styles.listItemSelected
@@ -64,6 +81,7 @@ class WindowAppList extends React.Component<
         className={classes}
         onClick={() => {
           this.setState({ selectedIndex: index });
+          this.listRef.current!.focus();
         }}
       >
         <p className={styles.processName}>Process: {item.processName}</p>
@@ -74,13 +92,45 @@ class WindowAppList extends React.Component<
 
   render() {
     const list = new Array<any>();
-    for (let i = 0; i < this.state.listData.length; i++) {
+    const length = this.props.storeFGM!.listAppToMonitor.length;
+    for (let i = 0; i < length; i++) {
       list.push(this.renderItem(i));
     }
 
+    let btnLeft = 412;
+    const e = this.listRef.current;
+    if (e) {
+      if (e.scrollHeight > e.clientHeight) {
+        btnLeft = 394;
+      }
+    }
+
     return (
-      <div style={{ width: '100%', height: '100%', overflowY: 'auto' }}>
+      <div
+        tabIndex={0}
+        ref={this.listRef}
+        style={{
+          width: '100%',
+          height: '100%',
+          overflowX: 'hidden',
+          overflowY: 'auto',
+          position: 'relative'
+        }}
+        onKeyDown={this.handleKeyDown}
+        onBlurCapture={e => {
+          console.log('onblur');
+        }}
+      >
         {list}
+        <FloatingButton
+          position='fixed'
+          left={btnLeft}
+          top={87}
+          icon='add'
+          intent='danger'
+          scale={1.3}
+          onClick={() => {}}
+        />
       </div>
     );
   }
