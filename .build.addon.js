69885cce13e36f6a84048024787876d5fe0836e9
RomiTT <chikyom.kim@gmail.com>
2019-07-16 03:38:54 +0900

    Add script which build launch_at_logon addon module.

--- .build.addon.js
+++ .build.addon.js
@@ -1,5 +1,11 @@
 var shell = require('shelljs');
+
 shell.cd('./native_addons/frameless_game_mode');
 shell.exec('yarn clean');
 shell.exec('yarn build');
+
+shell.cd('../launch_at_logon');
+shell.exec('yarn clean');
+shell.exec('yarn build');
+
 shell.cd('../..');
