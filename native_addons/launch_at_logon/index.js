d5d33bb1f5f2ea8a35fd33386862cf0ebbba89e4
RomiTT <chikyom.kim@gmail.com>
2019-07-17 03:14:24 +0900

    Test script was updated.

--- native_addons/launch_at_logon/index.js
+++ native_addons/launch_at_logon/index.js
@@ -2,4 +2,24 @@ const LaunchAtLogon = require('./build/Release/launchAtLogon.node');
 
 console.log('launchAtLogon', LaunchAtLogon);
 
+const taskName = 'Frameless Game Mode';
+const appPath =
+  'D:\\D-Projects\\GitHub\\frameless-game-mode\\dist\\win-unpacked\\Frameless Game Mode.exe';
+const appArgs = '--silent';
+
+LaunchAtLogon.initialize();
+
+LaunchAtLogon.set(false, taskName, appPath, appArgs, () => {
+  LaunchAtLogon.get(taskName, result => {
+    console.log('FALSE, LaunchAtLogon.get() => ', result);
+  });
+});
+
+LaunchAtLogon.set(true, taskName, appPath, appArgs, () => {
+  LaunchAtLogon.get(taskName, result => {
+    console.log('FALSE, LaunchAtLogon.get() => ', result);
+    LaunchAtLogon.uninitialize();
+  });
+});
+
 module.exports = LaunchAtLogon;
