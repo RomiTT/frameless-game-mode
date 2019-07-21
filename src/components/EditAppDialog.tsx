5d6093b892ab5ba1dc16f5dac945c99fc7fe9977
RomiTT <chikyom.kim@gmail.com>
2019-07-22 02:46:45 +0900

    Created EditAppDialog component.

---
+++ src/components/EditAppDialog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './EditAppDialog.module.scss';
+
+interface IProps {}
+
+interface IState {}
+
+class EditAppDialog extends React.PureComponent<IProps, IState> {
+  render() {
+    return <div />;
+  }
+}
