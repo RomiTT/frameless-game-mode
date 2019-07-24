4205cc5917be653dd45834b6838ac3f3e366ea06
RomiTT <chikyom.kim@gmail.com>
2019-07-24 12:21:39 +0900

    Fixed a bug.

--- public/electron.js
+++ public/electron.js
@@ -104,6 +104,43 @@ function createWindow() {
 
   if (isDev === false) {
     autoUpdater.checkForUpdates();
+
+    const contents = mainWindow.webContents;
+
+    autoUpdater.on('update-available', function() {
+      console.log('A new update is available');
+      contents.send('updater-message', 'A new update is available');
+    });
+
+    autoUpdater.on('checking-for-update', function() {
+      console.log('Checking-for-update');
+      contents.send('updater-message', 'Checking for Update..');
+    });
+
+    autoUpdater.on('error', function(error) {
+      console.log('error');
+      console.error(error);
+      contents.send('updater-message', 'Got Error');
+    });
+
+    autoUpdater.on('download-progress', function(bytesPerSecond, percent, total, transferred) {
+      console.log(`${bytesPerSecond}, ${percent}, ${total}, ${transferred}`);
+      contents.send(
+        'updater-message',
+        `download progress : ${bytesPerSecond}, ${percent}, ${total}, ${transferred}`
+      );
+    });
+
+    autoUpdater.on('update-downloaded', function(event) {
+      console.log('update-downloaded');
+      console.log(event);
+      contents.send('updater-message', 'update-downloaded');
+    });
+
+    autoUpdater.on('update-not-available', function() {
+      console.log('update-not-available');
+      contents.send('updater-message', 'update-not-available');
+    });
   }
 }
 
@@ -155,43 +192,4 @@ if (!gotTheLock) {
       createWindow();
     }
   });
-
-  if (isDev === false) {
-    const contents = mainWindow.webContents;
-
-    autoUpdater.on('update-available', function() {
-      console.log('A new update is available');
-      contents.send('updater-message', 'A new update is available');
-    });
-
-    autoUpdater.on('checking-for-update', function() {
-      console.log('Checking-for-update');
-      contents.send('updater-message', 'Checking for Update..');
-    });
-
-    autoUpdater.on('error', function(error) {
-      console.log('error');
-      console.error(error);
-      contents.send('updater-message', 'Got Error');
-    });
-
-    autoUpdater.on('download-progress', function(bytesPerSecond, percent, total, transferred) {
-      console.log(`${bytesPerSecond}, ${percent}, ${total}, ${transferred}`);
-      contents.send(
-        'updater-message',
-        `download progress : ${bytesPerSecond}, ${percent}, ${total}, ${transferred}`
-      );
-    });
-
-    autoUpdater.on('update-downloaded', function(event) {
-      console.log('update-downloaded');
-      console.log(event);
-      contents.send('updater-message', 'update-downloaded');
-    });
-
-    autoUpdater.on('update-not-available', function() {
-      console.log('update-not-available');
-      contents.send('updater-message', 'update-not-available');
-    });
-  }
 }
